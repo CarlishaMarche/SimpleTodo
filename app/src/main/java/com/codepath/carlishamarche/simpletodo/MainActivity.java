a18807267865dd4164fc350845cb0b4654fce3e3
Carlisha Mitchell <59887531+CarlishaMarche@users.noreply.github.com>
2020-01-21 13:35:38 -0600

    Initial commit

---
+++ app/src/main/java/com/codepath/carlishamarche/simpletodo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.codepath.carlishamarche.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<string> items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
